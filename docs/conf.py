a03cc9636b1f1a4ad401dafb2f6783f38888e7ef
Xiyuan Zhang <97223450+Xiyuan0315@users.noreply.github.com>
2025-09-10 20:29:26 +0300

    update user guide

--- docs/conf.py
+++ docs/conf.py
@@ -1,17 +1,17 @@
 import os, sys
-sys.path.append(os.path.abspath("../"))  # 可选
+sys.path.append(os.path.abspath("../"))
 
 language = "en"
 
-# 扩展
 extensions = [
-    # "myst_parser",            # 支持 Markdown (commented out since we're using RST)
-    "sphinx.ext.autodoc",     # 可选：从 Python docstring 生成文档
-    "sphinx.ext.napoleon",    # 可选：支持 Google/NumPy 风格注释
-    "sphinx.ext.viewcode",    # 可选：显示源码链接
+    "sphinx.ext.autodoc",    
+    "sphinx.ext.napoleon", 
+    "sphinx.ext.viewcode",  
 ]
 # myst_enable_extensions = ["colon_fence"]
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.history']
+html_baseurl = "/guanaco-viz/"
+
 master_doc = "index"
 
 source_suffix = ".rst"
